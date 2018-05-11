ceb52725b54c3e91ba1d7e1cd2e67e9c853030ad
techsubhash <39178712+techsubhash@users.noreply.github.com>
2018-05-11 09:03:24 +0530

    log_in
    Termux log in and password

---
+++ log.py
@@ -0,0 +1,34 @@
+import os
+import time
+
+from getpass import getpass
+
+def menu():
+    os.system('clear')
+    print('\033[1;36;40m  v.1.1  [<<<---------Best hacker ---------->>>]')   
+    print("")
+    
+    print('\033[1;92m    786 => bismi-llāhir-raḥmānir-raḥīm')
+    print("\033[1;93m")
+    print("  << ---------Subhash Ladumor---------->>")
+    print("\033[1;96m")
+    x = input('\033[1;92mUsername \033[1;93m: ')
+    print("")
+    o = input("Are you want to show your password [ y/n ] : ")
+    print("")
+    if o=="y":
+        e = input('\033[1;92mPassword \033[1;93m: ')
+    elif o=="n":
+        e = getpass('\033[1;92mPassword \033[1;93m: ')
+
+    if x=="muju" and e=="123":
+        print('')
+        print('    welcome ========>',x)
+        print("")
+    else:
+         print("")
+         print("\033[1;91m       You can't Access my Termux privacy 😜")
+         time.sleep(2)
+         os.system('ps')
+         os.system('killall -9 com.termux')
+menu()
